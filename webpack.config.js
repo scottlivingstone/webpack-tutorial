8bcb9162a755676deabc1dda8c5d9cfcc1d02a61
Scott Livingstone <scottlivingstone@users.noreply.github.com>
2017-11-27 20:56:57 +0000

    add image loaders

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,35 @@ let config = {
                     use: ['css-loader', 'sass-loader'], // use these loaders
                     fallback: 'style-loader' // fallback for any css not loaded
                 }) // end extract
-            }
+            },
+            {
+                test: /\.jsx$/, // files ending with .jsx
+                exclude: /node_modules/, // exclude the node_modules directory
+                loader: "babel-loader" // use this (babel-core) loader
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: ['file-loader?context=src/assets/images/&name=images/[path][name].[ext]', {
+                    loader: 'image-webpack-loader',
+                    query: {
+                        mozjpeg: {
+                            progressive: true,
+                        },
+                        gifsicle: {
+                            interlaced: false,
+                        },
+                        optipng: {
+                            optimizationLevel: 4,
+                        },
+                        pngquant: {
+                            quality: '75-90',
+                            speed: 3,
+                        },
+                    },
+                }],
+                exclude: /node_modules/,
+                include: __dirname,
+            },
         ] // end rules
     },
     plugins: [
